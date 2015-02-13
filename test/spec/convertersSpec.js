80d7e0b5ed8abb6a8bcb3f2bc02423e0e3003503
Tom Mårdh <tom.mardh@softhouse.se>
2015-02-13 21:52:43 +0100

    Added structure for automated tests

---
+++ test/spec/convertersSpec.js
@@ -0,0 +1,48 @@
+describe("Converters", function() {
+  
+  beforeEach(function() {
+    
+  });
+
+  approveIt("should approve object", function(approvals) {
+    var object = {};
+    object.id = 100;
+    object.name = "testObject";
+    object.href = "http://url.com/test.txt";
+    approvals.verify(object);
+  });
+
+  approveIt("should not be same as approved", function(approvals) {
+    var object = {};
+    object.id = 101;
+    object.name = "testObject";
+    object.href = "http://url.com/test.txt";
+    approvals.verify(object);
+  });
+
+  approveIt("should have no approved output yet", function(approvals) {
+    var object = {};
+    object.id = 100;
+    object.name = "testObject";
+    object.href = "http://url.com/test.txt";
+    approvals.verify(object);
+  });
+
+  approveIt("failing regular test", function(approvals) {
+    var object = {};
+    object.id = 101;
+    object.name = "testObject";
+    object.href = "http://url.com/test.txt";
+    approvals.verify(object);
+    //expect(object).toBe({"id":101,"name":"testObject","href":"http://url.com/test.txt"});
+  });
+
+  //regular test
+  it("should convert number to int string", function() {
+    var number = 10.3;
+    var integerString = "" + Math.floor(number);
+    expect(integerString).toEqual("10");
+  });
+});
+
+ 
