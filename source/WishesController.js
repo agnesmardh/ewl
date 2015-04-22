daa812eeb5b2e4e80250c76e365a8820d044d3f8
Tom Mårdh <tom.mardh@softhouse.se>
2015-04-22 21:01:16 +0200

    Added possibility for different buyers

--- source/WishesController.js
+++ source/WishesController.js
@@ -1,15 +1,25 @@
 app.controller("WishesController", function() {
     var wishlist=this;
-    wishlist.wishes=['books','movies','headphones'];
+    wishlist.wishes=[{wish:"Books",buyer:"Agnes",checkWish:true},{wish:"Headphones",buyer:"Tom",checkWish:true},{wish:"Movies",buyer:"",checkWish:false}];
             
     wishlist.addWish=function(){
-      wishlist.wishes.push(wishlist.newWish);
+      wishlist.wishes.push({wish:wishlist.newWish, buyer:"",checkWish:false});
       wishlist.newWish="";
     };
     
     wishlist.deleteWish = function ( idx ) {
       wishlist.wishes.splice(idx, 1 );
     };
+
+    wishlist.toggleBought = function ( idx, sBuyer ) {
+      
+      if (wishlist.wishes[idx].checkWish) {
+        wishlist.wishes[idx].buyer = sBuyer;
+      } else {
+        wishlist.wishes[idx].buyer = "";
+      };
+      //wishlist.wishes[idx].checkWish = ~wishlist.wishes[idx].checkWish;
+    }; 
 
   });
 
